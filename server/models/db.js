f529572ab1e260df6e6819467f88d04456a5e515
rognstadragnar <rognstad.ragnar@gmail.com>
2016-11-18 15:31:56 +0100

    .

--- server/models/db.js
+++ server/models/db.js
@@ -1,5 +1,4 @@
 import Sequelize from 'sequelize';
-//import config from '../../config.json';
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
@@ -13,40 +12,3 @@ export const sequelize = new Sequelize(
     dialect: 'mysql'
     } 
 );
-/*
- 'ragnaar',
-  DB_PASSWORD: 'GzwfSLZI',
-  DB_HOST: 'mysql.stud.iie.ntnu.no',
-  DB_PORT: '3306',
-  JWT_SECRET: 'wowsuchdogejwtsecret',
-  SESSION_SECRET: 'omfgthisissoooosecretlool' }
-
-  
-export const sequelize = new Sequelize(
-    'ragnaar',
-    'ragnaar',
-    'GzwfSLZI', {
-    host: 'mysql.stud.iie.ntnu.no',
-    dialect: 'mysql'
-    } 
-);
-
-*//*
-(function asd(){
-    console.log(
-        process.env.DB_DATABASE,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,    
-        {host: process.env.DB_HOST}
-    )
-})()
-
-export const sequelize = new Sequelize(
-    process.env.DB_DATABASE,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST
-
-    }
-)*/
-
