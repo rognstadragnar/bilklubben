5fbf8a0c4a4d6eb36d18c5890fc37067ebda9f36
rognstadragnar <rognstad.ragnar@gmail.com>
2016-12-02 09:22:43 +0100

    -p

---
+++ webpack.production.config.js
@@ -0,0 +1,37 @@
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        path.join(__dirname, './client/assets/scripts/main.js')
+    ],
+    output: {
+        path: path.join(__dirname, './client/assets/scripts/'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                include: [
+                    path.join(__dirname, 'client'),
+                ],
+                //exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    ],
+    resolve: {
+        extensions: [ '', '.js', '.jsx']
+    },
+}
